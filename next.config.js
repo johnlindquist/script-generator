60672a1dfd3737f8f1ed17a5d066d797fd6f26c5
johnlindquist <johnlindquist@gmail.com>
2024-12-30 16:58:52 -0700

    Refactor image domain configuration in next.config.js
    - Updated the images configuration to use remotePatterns instead of domains for better handling of external image sources.
    - This change enhances the flexibility and security of image loading in the application, aligning with best practices for Next.js.
    
    These changes collectively improve the configuration for image handling, ensuring a more robust setup.

--- next.config.js
+++ next.config.js
@@ -8,7 +8,16 @@ const nextConfig = {
     return config
   },
   images: {
-    domains: ['avatars.githubusercontent.com', 'github.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+    ],
   },
 }
 
