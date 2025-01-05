cfa135d1950b0c593d67e9168c470d38e0bf950d
johnlindquist <johnlindquist@gmail.com>
2025-01-04 17:34:40 -0700

    feat: improve sponsor UI responsiveness
    - Hide sponsor background on mobile screens (show md+)
    
    - Hide sponsor ribbon on mobile (show xl+)
    
    - Merge SponsorRibbon into SponsorBackground component

--- components/SponsorBackground.tsx
+++ components/SponsorBackground.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from 'react'
 import StarIcon from './StarIcon'
+import Link from 'next/link'
+import { StarIcon as HeroStarIcon } from '@heroicons/react/24/solid'
 
 interface User {
   username: string | null
@@ -118,8 +120,24 @@ export default function SponsorBackground() {
 
   return (
     <>
+      {/* Sponsor Ribbon */}
+      <div className="absolute left-0 top-0 h-32 w-64 overflow-hidden z-10 hidden xl:block">
+        <div className="absolute -left-4 top-10 -translate-x-8 -rotate-[30deg]">
+          <div className="relative z-10 w-72 bg-amber-300/70 py-1 text-center shadow-lg">
+            <Link
+              href="https://github.com/sponsors/johnlindquist/sponsorships?sponsor=johnlindquist&tier_id=235205"
+              className="inline-flex items-center justify-center gap-1 text-sm font-bold text-black/90"
+            >
+              <HeroStarIcon className="h-3 w-3" />
+              Thanks to Our Sponsors!
+              <HeroStarIcon className="h-3 w-3" />
+            </Link>
+          </div>
+        </div>
+      </div>
+
       {/* Sponsor circles */}
-      <div className="absolute inset-0 overflow-hidden" style={{ zIndex: 0 }}>
+      <div className="absolute inset-0 overflow-hidden hidden md:block" style={{ zIndex: 0 }}>
         {/* Radial gradient overlay for center fade-out effect */}
         <div className="gradient-overlay" />
 
